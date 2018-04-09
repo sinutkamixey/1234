f818333bbd4a5a09d33f439688575c520674afd1
sinutkamixey <toma.020396@gmail.com>
2018-04-09 16:42:14 +0700

    fvlee;gb

--- pages/user.php
+++ pages/user.php
@@ -3,12 +3,12 @@ return function ($request, $response, $service, $app) {
 	$userId = $request->param('id');
 	$db=$app->db();
 	$query=$db->prepare('SELECT * FROM users where id=:id');
-	$query->exequte(['id'=>$userId]);
+	$query->execute(['id'=>$userId]);
 	$user=$query->fetch();
 	if ($user) {
 		$service->render(
 			'views/user.php',
-			['users'=>$users]
+			['users'=>$user]
 		);
 	}
 	else {
